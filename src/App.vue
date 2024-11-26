ff77c2ef0333adeca8354c0480d4fc1311e18390
Nishihara Tsubasa <nishihara.tsubasa77@gmail.com>
2024-11-26 22:01:46 +0900

    作成開始

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import HelloWorld from './components/HelloWorld.vue'
+import NumberInput from "./components/NumberInput.vue";
+
+const debtRows = ref<number>(100);
+const interest = ref<number>(15)
+const payback = ref<number>(10);
+</script>
+
+<template>
+  <div>
+    <NumberInput label="負債行数" unit="行" v-model.number="debtRows" />
+
+  </div>
+  <div>
+    <NumberInput label="利子" unit="%" v-model.number="interest" />
+  </div>
+  <div>
+    <NumberInput label="毎月の返済行数" unit="行" v-model.number="payback" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/NumberInput.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+type Props = {
+  label: string;
+  unit?:string;
+};
+const props = defineProps<Props>();
+const model = defineModel();
+</script>
+<template>
+  <label>
+    <span class="label">{{ label }}</span>
+    <input type="number" v-model.number="model" />
+    <span v-if="unit">{{ unit }}</span>
+  </label>
+</template>
